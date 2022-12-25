1bee57184957544d6597e7d5891c44227a74da35
AKINGKORAI <118323297+AKINGKORAI@users.noreply.github.com>
2022-12-25 08:19:59 +0500

    Update JXB.py

--- JXB.py
+++ JXB.py
@@ -1,1 +1,17 @@
-import JXB
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from JXB import Legend
+    Legend()
+elif bit == '32bit':
+    from JXB32 import Legend
+    Legend()
+else:
+    print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
+    os.system('exit')
+ 
